95e1925679ddbda7361d1cf4c8fdf01c57a53fa1
unknown <rijal.1344@gmail.com>
2023-03-07 09:53:23 +0700

    add validation create akun

--- resources/js/Pages/Akun/Create.vue
+++ resources/js/Pages/Akun/Create.vue
@@ -9,25 +9,30 @@
                         <div class="card-body">
                             <form @submit.prevent="form.post('/akun')" class="mt-3">
                                 <div class="mb-3">
-                                    <p>{{ form.errors }}</p>
+                                    <!-- <p>{{ form.errors.name }}</p> -->
+                                    <!-- <p>{{ isInvalid }}</p> -->
                                     <label for="name" class="form-label">Nama</label>
-                                    <input type="text" v-model="form.name" class="form-control" id="name">
+                                    <input type="text" v-model="form.name" :class="{ 'is-invalid': form.errors.name }" class="form-control" id="name" required>
+                                    <div v-if="form.errors.name" class="invalid-feedback">{{ form.errors.name }}</div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="username" class="form-label">Username</label>
-                                    <input type="text" v-model="form.username" class="form-control" id="username">
+                                    <input type="text" v-model="form.username" :class="{ 'is-invalid': form.errors.username }" class="form-control" id="username" required>
+                                    <div v-if="form.errors.username" class="invalid-feedback">{{ form.errors.username }}</div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="role" class="form-label">Role</label>
-                                    <select class="form-select" v-model="form.role" aria-label="Default select example">
+                                    <select class="form-select" v-model="form.role" :class="{ 'is-invalid': form.errors.role }" aria-label="Default select example" required>
                                         <option selected value="">Open this select menu</option>
                                         <option value="1">Admin</option>
                                         <option value="2">Bendahara</option>
                                     </select>
+                                    <div v-if="form.errors.role" class="invalid-feedback">{{ form.errors.role }}</div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="password" class="form-label">Password</label>
-                                    <input type="text" v-model="form.password" class="form-control" id="password">
+                                    <input type="text" v-model="form.password" :class="{ 'is-invalid': form.errors.password }" class="form-control" id="password" required>
+                                    <div v-if="form.errors.password" class="invalid-feedback">{{ form.errors.password }}</div>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
@@ -39,6 +44,7 @@
     </main>
 </template>
 <script>
+import { ref } from 'vue'
 import { useForm } from '@inertiajs/vue3'
 import NavBar from '../components/NavBar.vue'
 export default {
